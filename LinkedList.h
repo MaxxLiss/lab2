94a226258999c0cad1b712323523fd8db980456e
Maxim <maximlisa@yandex.ru>
2024-05-19 16:31:13 +0300

    LinkedList: some refactoring

--- LinkedList.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by user on 19.05.2024.
-//
-
-#ifndef LAB2_LINKEDLIST_H
-#define LAB2_LINKEDLIST_H
-
-#endif //LAB2_LINKEDLIST_H
---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,16 @@
+#ifndef LAB2_LINKEDLIST_H
+#define LAB2_LINKEDLIST_H
+
+template<class T>
+class LinkedList {
+private:
+    class Node {
+
+    };
+
+public:
+
+private:
+};
+
+#endif //LAB2_LINKEDLIST_H
