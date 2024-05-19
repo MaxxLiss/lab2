f7ccfba1f9300c3688bb445619f749af2d2da1e1
Maxim <maximlisa@yandex.ru>
2024-05-19 14:58:26 +0300

    DynamicArray ended

---
+++ DynamicArray/DynamicArray.cpp
@@ -0,0 +1,65 @@
+#include "DynamicArray.h"
+
+template<class T>
+DynamicArray<T>::DynamicArray(size_t size) : size_(size), data_(new T[size_]) {
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = T();
+    }
+}
+
+template<class T>
+DynamicArray<T>::DynamicArray(T* items, size_t count) : size_(count), data_(new T[size_]) {
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = items[i];
+    }
+}
+
+template<class T>
+DynamicArray<T>::DynamicArray(const DynamicArray<T> &dynamicArray) : size_(dynamicArray.size_), data_(new T[size_]) {
+//     meow meow meow mewoew meoowoowmeow
+    for (size_t i = 0; i < size_; ++i) {
+        data_[i] = dynamicArray.data_[i];
+    }
+}
+
+template<class T>
+DynamicArray<T>::~DynamicArray() {
+    delete[] data_;
+}
+
+template<class T>
+T DynamicArray<T>::Get(size_t index) const {
+    return data_[index];
+}
+
+template<class T>
+size_t DynamicArray<T>::GetSize() const {
+    return size_;
+}
+
+template<class T>
+void DynamicArray<T>::Set(size_t index, T value) {
+    data_[index] = value;
+}
+
+template<class T>
+void DynamicArray<T>::Resize(size_t newSize) {
+    T* newData = new T[newSize];
+    if (newSize < this->size_) {
+        for (size_t i = 0; i < newSize; ++i) {
+            newData[i] = data_[i];
+        }
+    } else {
+        for (size_t i = 0; i < this->size_; ++i) {
+            newData[i] = data_[i];
+        }
+        for (size_t i = this->size_; i < newSize; ++i) {
+            newData[i] = T();
+        }
+    }
+
+    delete[] data_;
+
+    this->data_ = newData;
+    this->size_ = newSize;
+}
---
+++ DynamicArray/DynamicArray.h
@@ -0,0 +1,30 @@
+#ifndef LAB2_DYNAMICARRAY_H
+#define LAB2_DYNAMICARRAY_H
+
+#include <cstddef>
+
+template<class T>
+class DynamicArray {
+public:
+    DynamicArray(T* items, size_t count);
+
+    explicit DynamicArray(size_t size);
+
+    DynamicArray(const DynamicArray<T>& dynamicArray);
+
+    ~DynamicArray();
+
+    T Get(size_t index) const;
+
+    size_t GetSize() const;
+
+    void Set(size_t index, T value);
+
+    void Resize(size_t newSize);
+
+private:
+    size_t size_;
+    T* data_;
+};
+
+#endif //LAB2_DYNAMICARRAY_H
