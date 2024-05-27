febeeeeaf925280fca1851bf3cb68fc1cd99ea33
Maxim <maximlisa@yandex.ru>
2024-05-27 21:07:58 +0300

    ArraySequence: ArraySequence was added

---
+++ ArraySequence/ArraySequence/ArraySequence.h
@@ -0,0 +1,28 @@
+#ifndef LAB2_ARRAYSEQUENCE_H
+#define LAB2_ARRAYSEQUENCE_H
+
+#include "../../Sequence/Sequence.cpp"
+#include "../../DynamicArray/DynamicArray.cpp"
+
+template<class T>
+class ArraySequence : public Sequence<T> {
+    ArraySequence(T* items, size_t count);
+
+    ArraySequence();
+
+    ArraySequence(const ArraySequence<T>& arraySequence);
+
+    ~ArraySequence() override = default;
+
+    T GetFirst() override;
+
+    T GetLast() override;
+
+    T Get() override;
+
+private:
+    DynamicArray<T> data_;
+};
+
+
+#endif //LAB2_ARRAYSEQUENCE_H
