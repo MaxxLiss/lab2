9da2abda1afb9bbd1b63bc65dd0395ba3368f340
MaxxLiss <maximlisa@yandex.ru>
2024-05-28 00:50:36 +0300

    LinkedList: tests namespace added

--- Tests/LinkedListTests.cpp
+++ Tests/LinkedListTests.cpp
@@ -2,193 +2,196 @@
 #include <cassert>
 #include "../LinkedList/LinkedList.h"
 
-void testEmptyList() {
-    LinkedList<int> linkedList;
-    assert(linkedList.GetLength() == 0);
-    assert(linkedList.IsEmpty() == 1);
-    try {
-        linkedList.GetFirst();
-        assert(true);
-    } catch (std::out_of_range& e) {}
-    try {
-        linkedList.GetLast();
-        assert(true);
-    } catch (std::out_of_range& e) {}
-    for (int i = -5; i < 10; ++i) {
+namespace LinkedListTest {
+
+    void testEmptyList() {
+        LinkedList<int> linkedList;
+        assert(linkedList.GetLength() == 0);
+        assert(linkedList.IsEmpty() == 1);
         try {
-            linkedList.Get(i);
+            linkedList.GetFirst();
             assert(true);
-        } catch (...) {}
+        } catch (std::out_of_range &e) {}
         try {
-            linkedList.InsertAt(5, i);
-        } catch (...) {}
-        try {
-            linkedList.GetSubList(i, i + 1);
-        } catch (...) {}
-    }
-}
-
-void testAdd() {
-    LinkedList<int> linkedList;
-
-    assert(linkedList.GetLength() == 0);
-    assert(linkedList.IsEmpty() == 1);
-
-    for (int i = 0; i < 5; ++i) {
-        linkedList.Append(i);
-        assert(linkedList.GetLast() == i);
-    }
-    for (int i = 0; i < 5; ++i) {
-        assert(linkedList.Get(i) == i);
-    }
-    assert(linkedList.GetFirst() == 0);
-
-    assert(linkedList.IsEmpty() == 0);
-    assert(linkedList.GetLength() == 5);
-
-    linkedList.Clear();
-
-    assert(linkedList.GetLength() == 0);
-    assert(linkedList.IsEmpty() == 1);
-
-    for (int i = 0; i < 5; ++i) {
-        linkedList.Prepend(i);
-        assert(linkedList.GetFirst() == i);
-    }
-    for (int i = 0; i < 5; ++i) {
-        assert(linkedList.Get(5 - i - 1) == i);
-    }
-    assert(linkedList.GetLast() == 0);
-
-    assert(linkedList.IsEmpty() == 0);
-    assert(linkedList.GetLength() == 5);
-}
-
-void testCopyConstructor() {
-    LinkedList<int> linkedList;
-    for (int i = 0; i < 5; ++i) {
-        linkedList.Append(i);
-    }
-    LinkedList<int> linkedListCopy(linkedList);
-
-    assert(linkedListCopy.IsEmpty() == 0);
-    assert(linkedListCopy.GetLength() == 5);
-
-    assert(linkedListCopy.GetFirst() == 0);
-    assert(linkedListCopy.GetLast() == 4);
-
-    for (int i = 0; i < 5; ++i) {
-        assert(linkedListCopy.Get(i) == i);
-    }
-}
-
-void testCopyArrayConstructor() {
-    int data[] = {0, 1, 2, 3, 4};
-    LinkedList<int> linkedList(data, 5);
-
-    assert(linkedList.IsEmpty() == 0);
-    assert(linkedList.GetLength() == 5);
-
-    assert(linkedList.GetFirst() == 0);
-    assert(linkedList.GetLast() == 4);
-
-    for (int i = 0; i < 5; ++i) {
-        assert(linkedList.Get(i) == i);
-    }
-}
-
-void testSubList() {
-    int data[] = {0, 1, 2, 3, 4};
-    LinkedList<int> linkedList(data, 5);
-
-    for (int i = -5; i < 10; ++i) {
-        try {
-            linkedList.GetSubList(i, i + 5);
-        } catch (...) {}
+            linkedList.GetLast();
+            assert(true);
+        } catch (std::out_of_range &e) {}
+        for (int i = -5; i < 10; ++i) {
+            try {
+                linkedList.Get(i);
+                assert(true);
+            } catch (...) {}
+            try {
+                linkedList.InsertAt(5, i);
+            } catch (...) {}
+            try {
+                linkedList.GetSubList(i, i + 1);
+            } catch (...) {}
+        }
     }
 
-    LinkedList<int>* subLinkedList(linkedList.GetSubList(1, 3));
+    void testAdd() {
+        LinkedList<int> linkedList;
 
-    assert(subLinkedList->IsEmpty() == 0);
-    assert(subLinkedList->GetLength() == 3);
+        assert(linkedList.GetLength() == 0);
+        assert(linkedList.IsEmpty() == 1);
 
-    int excepted[] = {1, 2, 3};
-    for (int i = 0; i < 3; ++i) {
-        assert(subLinkedList->Get(i) == excepted[i]);
+        for (int i = 0; i < 5; ++i) {
+            linkedList.Append(i);
+            assert(linkedList.GetLast() == i);
+        }
+        for (int i = 0; i < 5; ++i) {
+            assert(linkedList.Get(i) == i);
+        }
+        assert(linkedList.GetFirst() == 0);
+
+        assert(linkedList.IsEmpty() == 0);
+        assert(linkedList.GetLength() == 5);
+
+        linkedList.Clear();
+
+        assert(linkedList.GetLength() == 0);
+        assert(linkedList.IsEmpty() == 1);
+
+        for (int i = 0; i < 5; ++i) {
+            linkedList.Prepend(i);
+            assert(linkedList.GetFirst() == i);
+        }
+        for (int i = 0; i < 5; ++i) {
+            assert(linkedList.Get(5 - i - 1) == i);
+        }
+        assert(linkedList.GetLast() == 0);
+
+        assert(linkedList.IsEmpty() == 0);
+        assert(linkedList.GetLength() == 5);
     }
 
-    assert(subLinkedList->GetFirst() == 1);
-    assert(subLinkedList->GetLast() == 3);
+    void testCopyConstructor() {
+        LinkedList<int> linkedList;
+        for (int i = 0; i < 5; ++i) {
+            linkedList.Append(i);
+        }
+        LinkedList<int> linkedListCopy(linkedList);
 
-    delete subLinkedList;
+        assert(linkedListCopy.IsEmpty() == 0);
+        assert(linkedListCopy.GetLength() == 5);
 
-    subLinkedList = linkedList.GetSubList(0, 4);
-    assert(subLinkedList->IsEmpty() == 0);
-    assert(subLinkedList->GetLength() == 5);
+        assert(linkedListCopy.GetFirst() == 0);
+        assert(linkedListCopy.GetLast() == 4);
 
-    for (int i = 0; i < 5; ++i) {
-        assert(subLinkedList->Get(i) == i);
+        for (int i = 0; i < 5; ++i) {
+            assert(linkedListCopy.Get(i) == i);
+        }
     }
 
-    assert(subLinkedList->GetFirst() == 0);
-    assert(subLinkedList->GetLast() == 4);
+    void testCopyArrayConstructor() {
+        int data[] = {0, 1, 2, 3, 4};
+        LinkedList<int> linkedList(data, 5);
 
-    delete subLinkedList;
-}
+        assert(linkedList.IsEmpty() == 0);
+        assert(linkedList.GetLength() == 5);
 
-void testConcat() {
-    LinkedList<int> emptyList1, emptyList2;
+        assert(linkedList.GetFirst() == 0);
+        assert(linkedList.GetLast() == 4);
 
-    auto* emptyResult = emptyList1.Concat(&emptyList2);
-
-    assert(emptyResult->IsEmpty() == 1);
-    assert(emptyResult->GetLength() == 0);
-
-    delete emptyResult;
-
-    int data1[] = {1, 2, 3, 4};
-    int data2[] = {5, 6, 7, 8};
-
-    int excepted[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    LinkedList<int> linkedList1(data1, 4), linkedList2(data2, 4);
-    auto *resultList = linkedList1.Concat(&linkedList2);
-
-    assert(resultList->IsEmpty() == 0);
-    assert(resultList->GetLength() == 8);
-
-    for (int i = 0; i < 8; ++i) {
-        assert(resultList->Get(i) == excepted[i]);
+        for (int i = 0; i < 5; ++i) {
+            assert(linkedList.Get(i) == i);
+        }
     }
 
-    assert(resultList->GetFirst() == 1);
-    assert(resultList->GetLast() == 8);
+    void testSubList() {
+        int data[] = {0, 1, 2, 3, 4};
+        LinkedList<int> linkedList(data, 5);
 
-    delete resultList;
-}
+        for (int i = -5; i < 10; ++i) {
+            try {
+                linkedList.GetSubList(i, i + 5);
+            } catch (...) {}
+        }
 
-void testInsertAt() {
-    int data[] = {1, 3, 5, 7, 9};
-    LinkedList<int> linkedList(data, 5);
-    for (int i = 0; i <= 10; i += 2) {
-        linkedList.InsertAt(i, i);
+        LinkedList<int> *subLinkedList(linkedList.GetSubList(1, 3));
+
+        assert(subLinkedList->IsEmpty() == 0);
+        assert(subLinkedList->GetLength() == 3);
+
+        int excepted[] = {1, 2, 3};
+        for (int i = 0; i < 3; ++i) {
+            assert(subLinkedList->Get(i) == excepted[i]);
+        }
+
+        assert(subLinkedList->GetFirst() == 1);
+        assert(subLinkedList->GetLast() == 3);
+
+        delete subLinkedList;
+
+        subLinkedList = linkedList.GetSubList(0, 4);
+        assert(subLinkedList->IsEmpty() == 0);
+        assert(subLinkedList->GetLength() == 5);
+
+        for (int i = 0; i < 5; ++i) {
+            assert(subLinkedList->Get(i) == i);
+        }
+
+        assert(subLinkedList->GetFirst() == 0);
+        assert(subLinkedList->GetLast() == 4);
+
+        delete subLinkedList;
     }
 
-    assert(linkedList.IsEmpty() == 0);
-    assert(linkedList.GetLength() == 11);
+    void testConcat() {
+        LinkedList<int> emptyList1, emptyList2;
 
-    for (int i = 0; i < 11; ++i) {
-        assert(linkedList.Get(i) == i);
+        auto *emptyResult = emptyList1.Concat(&emptyList2);
+
+        assert(emptyResult->IsEmpty() == 1);
+        assert(emptyResult->GetLength() == 0);
+
+        delete emptyResult;
+
+        int data1[] = {1, 2, 3, 4};
+        int data2[] = {5, 6, 7, 8};
+
+        int excepted[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        LinkedList<int> linkedList1(data1, 4), linkedList2(data2, 4);
+        auto *resultList = linkedList1.Concat(&linkedList2);
+
+        assert(resultList->IsEmpty() == 0);
+        assert(resultList->GetLength() == 8);
+
+        for (int i = 0; i < 8; ++i) {
+            assert(resultList->Get(i) == excepted[i]);
+        }
+
+        assert(resultList->GetFirst() == 1);
+        assert(resultList->GetLast() == 8);
+
+        delete resultList;
     }
 
-    assert(linkedList.GetFirst() == 0);
-    assert(linkedList.GetLast() == 10);
-}
+    void testInsertAt() {
+        int data[] = {1, 3, 5, 7, 9};
+        LinkedList<int> linkedList(data, 5);
+        for (int i = 0; i <= 10; i += 2) {
+            linkedList.InsertAt(i, i);
+        }
 
-void testLinkedList() {
-    testEmptyList();
-    testAdd();
-    testCopyConstructor();
-    testCopyArrayConstructor();
-    testSubList();
-    testConcat();
+        assert(linkedList.IsEmpty() == 0);
+        assert(linkedList.GetLength() == 11);
+
+        for (int i = 0; i < 11; ++i) {
+            assert(linkedList.Get(i) == i);
+        }
+
+        assert(linkedList.GetFirst() == 0);
+        assert(linkedList.GetLast() == 10);
+    }
+
+    void testLinkedList() {
+        testEmptyList();
+        testAdd();
+        testCopyConstructor();
+        testCopyArrayConstructor();
+        testSubList();
+        testConcat();
+    }
 }
