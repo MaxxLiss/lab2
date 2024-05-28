c1ea8d49630e666bf17e2e32dc31a9f12f0cb567
MaxxLiss <maximlisa@yandex.ru>
2024-05-28 14:24:13 +0300

    ListSequence: added

---
+++ ListSequence/ImmutableListSequence.h
@@ -0,0 +1,77 @@
+//
+// Created by Admin on 28.05.2024.
+//
+
+#ifndef LAB2_IMMUTABLELISTSEQUENCE_H
+#define LAB2_IMMUTABLELISTSEQUENCE_H
+
+#include "Sequence.h"
+#include "ListSequence.h"
+
+template<class T>
+class ImmutableListSequence : public Sequence<T> {
+public:
+    ImmutableListSequence(T* items, size_t count) : data_(items, count) {}
+
+    ImmutableListSequence() = default;
+
+    ImmutableListSequence(const ImmutableListSequence<T>& arraySequence) : data_(arraySequence.data_) {}
+
+    ImmutableListSequence(const Sequence<T>& sequence) {
+        data_.Reserve(sequence.GetLength());
+        for (size_t i = 0; i < sequence.GetLength(); ++i) {
+            data_.Append(sequence.Get(i));
+        }
+    }
+
+    ~ImmutableListSequence() override = default;
+
+    T GetFirst() const override {
+        return data_.GetFirst();
+    }
+
+    T GetLast() const override {
+        return data_.GetLength();
+    }
+
+    T Get(size_t index) const override {
+        return data_.Get(index);
+    }
+
+    Sequence<T> *GetSubSequence(size_t startIndex, size_t endIndex) const override {
+        return data_.GetSubSequence(startIndex, endIndex);
+    }
+
+    bool IsEmpty() const override {
+        return data_.IsEmpty();
+    }
+
+    size_t GetLength() const override {
+        return data_.GetLength();
+    }
+
+    Sequence<T> *Append(T item) override {
+        auto* res = new ImmutableListSequence<T>(*this);
+        return res->Append(item);
+    }
+
+    Sequence<T> *Prepend(T item) override {
+        auto* res = new ImmutableListSequence<T>(*this);
+        return res->Prepend(item);
+    }
+
+    Sequence<T> *InsertAt(T item, size_t index) override {
+        auto* res = new ImmutableListSequence<T>(*this);
+        return res->InsertAt(item, index);
+    }
+
+    Sequence<T> *Concat(Sequence<T> *list) const override {
+        return data_.Concat(list);
+    }
+
+private:
+    ListSequence<T> data_;
+};
+
+
+#endif //LAB2_IMMUTABLELISTSEQUENCE_H
